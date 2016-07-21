0a958918bcd0c99f09ea3b14dbe0326c067bba42
lmusco <lmusco@gmail.com>
2016-07-21 18:37:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -310,6 +310,7 @@ function receivedMessage(event) {
 
       default:
         sendTextMessage(senderID, messageText+" "+senderID);
+        console.log(message.sender);
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
